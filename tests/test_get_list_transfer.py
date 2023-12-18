5979dbba6732de1d46c9fd41b169cbaba76d5ad4
Kris <ssspers92@gmail.com>
2023-12-18 20:35:47 +0300

    add test_get_list_transfer_above_zero

--- tests/test_get_list_transfer.py
+++ tests/test_get_list_transfer.py
@@ -1,6 +1,7 @@
 import unittest
+from utils.get_list_transfer import get_last_transfer
 
-
-# class TestTransferInfo(unittest.TestCase):
-#     def test_get_list_transfer(self):
-#         self.assertEqual(get_last_transfer(DATA), 5)
+class TestTransferInfo(unittest.TestCase):
+    def test_get_list_transfer_above_zero(self):
+        self.assertNotEqual(get_last_transfer(), 0)
+        self.assertNotEqual(get_last_transfer(), None)
