7cb15b544040e1aac8876ba05bf449fa6df5275e
Kris <ssspers92@gmail.com>
2023-12-20 01:29:11 +0300

    create test test_get_last_five_transfers

--- tests/test_get_list_transfer.py
+++ tests/test_get_list_transfer.py
@@ -1,7 +1,15 @@
 import unittest
-from utils.get_list_transfer import Transfer
+from utils.Transfer import Transfer
+from utils.get_sorted_transfers import get_sorted_transfers
+
 
 class TestTransferInfo(unittest.TestCase):
     def test_get_list_transfer_above_zero(self):
-        self.assertNotEqual(Transfer().get_last_transfer(), 0)
-        self.assertNotEqual(Transfer().get_last_transfer(), None)
+        self.assertNotEqual(Transfer().get_list_transfer(), 0)
+        self.assertNotEqual(Transfer().get_list_transfer(), None)
+
+    def test_get_last_five_transfers(self):
+        self.assertNotEqual(get_sorted_transfers(Transfer().get_list_transfer()), Transfer().get_list_transfer())
+
+    # def test_get_last_five_transfers_count(self):
+    #     self.assertEqual(len(Transfer().last_info), 5)
