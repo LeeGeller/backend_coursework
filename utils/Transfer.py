3b68f0406b6d06f6ba31fb6a857cf41cc284bb12
Kris <ssspers92@gmail.com>
2023-12-20 01:42:27 +0300

    create get_last_info

--- utils/Transfer.py
+++ utils/Transfer.py
@@ -1,51 +1,45 @@
 from datetime import datetime as date
 import os
 import json
-from operator import itemgetter
+from utils.get_sorted_transfers import get_sorted_transfers
 
 
 class Transfer:
     def __init__(self):
         self.list_transfer = []
-        self.last_info = {}
 
     def __repr__(self):
         return (f"Return file with informstion\n"
                 f"about user:\n"
-                f"{self.list_transfer}\n"
-                f"Return 5 last transfers:\n"
-                f"{self.last_info}")
+                f"{self.list_transfer}\n")
 
     def get_list_transfer(self):
         with open(os.path.join('..', 'data', 'operations.json'), 'r', encoding='UTF-8') as list_data:
             self.list_transfer = json.loads(list_data.read().strip())
         return self.list_transfer
 
-    def get_last_five_transfers(self):
-        current_transfer = []
-        list_transfers = self.list_transfer
-        count = 5
-        while count > 0:
-            for i in range(len(self.list_transfer) - 1):
-                max_date = date.now()
-                next_date = list_transfers[i + 1].get('date')
+    def get_last_info(self):
+        get_sorted_transfers(self.list_transfer)
+        sorted_list = self.list_transfer
+        list_info = []
+        len_list_info = len(list_info)
 
-                if next_date:
-                    if list_transfers[i].get('state') == 'EXECUTED':
-                        first_date = list_transfers[i].get('date')
-                        date_1 = date.strptime(first_date, '%Y-%m-%dT%H:%M:%S.%f')
-                        date_2 = date.strptime(next_date, '%Y-%m-%dT%H:%M:%S.%f')
-                        if date_1 > date_2:
-                            max_date = date_1
-                            count -= 1
+        while len_list_info < 6:
 
-                current_transfer.append(list_transfers[i])
-                self.last_info = current_transfer
-            return self.last_info
+            for info in range(len(sorted_list) - 1):
+
+                date_str = sorted_list[info].get('date')
+                date_class = date.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
+                date_formatted = date_class.strftime("%d.%m.%Y")
+
+                list_info.extend([date_formatted, sorted_list[info]['description'],
+                                  sorted_list[info]['from'], '->', sorted_list[info]['to'],
+                                  sorted_list[info]['operationAmount']['amount'],
+                                  sorted_list[info]['operationAmount']['currency']['name']])
+                return list_info
 
 
 x = Transfer()
 x.get_list_transfer()
-x.get_last_five_transfers()
 
-print(x)
+print(x.get_last_info())
