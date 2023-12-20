f9a9c3d2a95e95c483244f8a29cf089a7b20c0fc
Kris <ssspers92@gmail.com>
2023-12-21 01:11:49 +0300

    Create main

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from src.Transfer import Transfer
+
+
+class main:
+    transfers = Transfer()
+    transfers.get_list_transfer()
+    transfers.get_true_sort_transfers()
+    transfers.code_check()
+
+    print(transfers.get_count_transfers())
+    print(transfers.get_last_info())
+
+
+if __name__ == '__main__':
+    main()
