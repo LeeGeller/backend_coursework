220590bcdff6c490aecd101f01e455c8da6363e8
Kris <ssspers92@gmail.com>
2023-12-22 01:55:39 +0300

    upgrade get_true_sort_transfers

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
+import pathlib
+
 from utils.Transfer import Transfer
 
 
 class main:
+    DATA = pathlib.Path('..', 'data', 'operations.json')
     transfers = Transfer()
-    list_transfers = transfers.get_list_transfer()
-    transfers.get_true_sort_transfers(list_transfers)
-    transfers.code_check(list_transfers)
+    get_list_transfers = transfers.get_list_transfer(DATA)
+    transfers.get_true_sort_transfers(transfers.list_transfer)
+    transfers.code_check(transfers.list_transfer)
 
-    print(transfers.get_last_info(list_transfers))
+    print(transfers.get_last_info(transfers.list_transfer))
 
 
 if __name__ == '__main__':
--- utils/Transfer.py
+++ utils/Transfer.py
@@ -1,6 +1,5 @@
 import pathlib
 from datetime import datetime as date
-import os
 import json
 
 
@@ -16,87 +15,82 @@ class Transfer:
                 f"Return last transfers:\n"
                 f"{self.last_info}")
 
-    def get_list_transfer(self):
+    def get_list_transfer(self, operations_path):
         """
         :return: get operations.json
         """
-        root_path = pathlib.Path('..')
-        operations_path = pathlib.Path('data', 'operations.json')
-        with open(pathlib.Path(root_path, operations_path), 'r', encoding='UTF-8') as list_data:
+        with open(pathlib.Path(operations_path), 'r', encoding='UTF-8') as list_data:
             self.list_transfer = json.loads(list_data.read().strip())
         return self.list_transfer
 
     def get_true_sort_transfers(self, list_):
         """
-        Sort transfer's list
-        :return: sorted list
+        clean and sort list without artefacts and with tru info
+        :param list_: list
+        :return: self.list_transfer
         """
         new_list = []
-        for date_ in list_:
-            str_date = date_.get('date')
-            if str_date is not None and date_.get('state') == 'EXECUTED':
-                cl_date = date.strptime(str_date, '%Y-%m-%dT%H:%M:%S.%f')
-                date_short = cl_date.strftime("%d.%m.%Y")
-                date_class = date.strptime(date_short, "%d.%m.%Y")
-                date_['date'] = date_class
-                new_list.append(date_)
+        for value in list_:
+            if value.get('state') == 'EXECUTED' and value.get('from'):
+                new_list.append(value)
+            else:
+                continue
 
-        self.list_transfer = sorted(new_list, key=lambda x: x['date'], reverse=True)
+        new_list = sorted(new_list, key=lambda x: x.get('date'), reverse=True)
+        self.list_transfer = new_list[:5]
+
         return self.list_transfer
 
     def code_check(self, list_):
         """
         Code information about from transfer
-        :return: coded info about from
+        :return: self.list_transfer
         """
-        for i in range(len(list_) - 1):
-            if list_[i].get('from'):
-                # Get str about date
-                str_info_from, str_info_to = list_[i].get('from'), list_[i].get('to')
-                list_info_from, list_info_to = str_info_from.split(), str_info_to.split()
 
-                # Get number and name card from
-                card_info_from = [info for info in list_info_from if info.isdigit()]
-                card_from = [info for info in list_info_from if info.isalpha()]
+        for i in list_:
+            # Get str about date and split these
+            str_info_from, str_info_to = i.get('from'), i.get('to')
+            list_info_from, list_info_to = str_info_from.split(), str_info_to.split()
 
-                # Get number and name card to
-                card_info_to = [info for info in list_info_to if info.isdigit()]
-                card_to = [info for info in list_info_to if info.isalpha()]
+            # Get number and name card from
+            card_info_from = [info for info in list_info_from if info.isdigit()]
+            card_from = [info for info in list_info_from if info.isalpha()]
 
-                # Join and code str info from
-                card_info_from, card_from = ''.join(card_info_from), ' '.join(card_from)
-                code_formatted = f'{card_from} {card_info_from[:4]} {card_info_from[4:6]}** ****{card_info_from[12:]}'
-                list_[i]['from'] = code_formatted
+            # Get number and name card to
+            card_info_to = [info for info in list_info_to if info.isdigit()]
+            card_to = [info for info in list_info_to if info.isalpha()]
 
-                # Join and code str info to
-                card_info_to, card_to = ' '.join(card_to), ''.join(card_info_to)
-                code_formatted = f'{card_info_to} **{card_to[len(card_to) - 4:]}'
-                list_[i]['to'] = code_formatted
+            # Join and code str info from
+            card_info_from, card_from = ''.join(card_info_from), ' '.join(card_from)
+            code_formatted = f'{card_from} {card_info_from[:4]} {card_info_from[4:6]}** ****{card_info_from[12:]}'
+            i['from'] = code_formatted
 
-                self.list_transfer = list_
+            # Join and code str info to
+            card_info_to, card_to = ' '.join(card_to), ''.join(card_info_to)
+            code_formatted = f'{card_info_to} **{card_to[len(card_to) - 4:]}'
+            i['to'] = code_formatted
+
+        self.list_transfer = list_
 
         return self.list_transfer
 
     def get_last_info(self, list_):
         """
         Get info about last transfers
-        :return: info
+        :return: self.last_info
         """
-        list_info = []
+        last_info = []
 
-        for i in range(len(list_) - 1):
-            if list_[i].get('from'):
-                date_str = list_[i].get('date')
+        for info in list_:
+            date_str = info.get('date')
+            date_class = date.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%f')
+            date_formatted = date_class.strftime("%d.%m.%Y")
 
-                date_formatted = date_str.strftime("%d.%m.%Y")
+            last_ = (f"{date_formatted} {info['description']}\n"
+                     f"{info['from']} -> {info['to']}\n"
+                     f"{info['operationAmount']['amount']} {info['operationAmount']['currency']['name']}\n")
+            last_info.append(last_)
 
-                last_info = (f"{date_formatted} {list_[i]['description']}\n"
-                             f"{list_[i]['from']} -> {list_[i]['to']}\n"
-                             f"{list_[i]['operationAmount']['amount']} {list_[i]['operationAmount']['currency']['name']}\n")
-                list_info.append(last_info)
-                if len(list_info) > 4:
-                    break
-
-        list_info = '\n'.join(list_info)
+        list_info = '\n'.join(last_info)
         self.last_info = list_info
         return self.last_info
