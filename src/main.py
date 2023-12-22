ea84606c63102eecb2c8f05309abc8520f611330
Kris <ssspers92@gmail.com>
2023-12-22 19:59:55 +0300

    add DATA and get_list_transfer(DATA)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import pathlib
 
 from utils.Transfer import Transfer
+from utils.get_list_transfer import get_list_transfer
 
 
 class main:
+    DATA = pathlib.Path('..', 'data', 'operations.json')
     transfers = Transfer()
-    transfers.get_list_transfer()
-    transfers.get_true_sort_transfers(transfers.list_transfer)
+    list_transfers = get_list_transfer(DATA)
+    transfers.get_true_sort_transfers(list_transfers)
     transfers.code_check(transfers.list_transfer)
 
     print(transfers.get_last_info(transfers.list_transfer))
