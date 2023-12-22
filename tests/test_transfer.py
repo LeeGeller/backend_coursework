0d0cf6ff4f1df8eb2f1a503a605354551cb729a4
Kris <ssspers92@gmail.com>
2023-12-22 22:31:10 +0300

    uprade test_get_list_transfer()

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -1,25 +1,23 @@
-import pathlib
-
 from utils.Transfer import Transfer
-from utils.get_list_transfer import get_list_transfer
 
 
-def test_get_list_transfer(test_data_file):
-    assert get_list_transfer('test_data.json') is not None
-    assert get_list_transfer('test_data.json') == test_data_file
+def test_get_list_transfer(fixture_data):
+    test_path = 'tests/test_data.json'
+    real_path = 'data/operations.json'
+    zero_path = ''
+
+    assert Transfer().get_list_transfer(test_path) == fixture_data
+    assert Transfer().get_list_transfer(real_path) == fixture_data
+    assert Transfer().get_list_transfer(zero_path) == "No such directory"
 
 
-def test_get_true_sort_transfers(test_data_file, test_sort_list):
-    assert Transfer().get_true_sort_transfers(test_data_file) == test_sort_list
-    assert Transfer().get_true_sort_transfers(test_data_file) != test_data_file
+def test_get_true_sort_transfers(fixture_data, test_sort_list):
+    assert Transfer().get_true_sort_transfers(fixture_data) == test_sort_list
 
 
 def test_code_check(test_info_code, test_sort_list):
     assert Transfer().code_check(test_sort_list) == test_info_code
-    assert len(Transfer().code_check(test_sort_list)) == len(test_info_code)
-    assert type(Transfer().code_check(test_sort_list)) == type(test_info_code)
 
 
 def test_get_last_info(test_sort_list, test_last_info_data):
     assert Transfer().get_last_info(test_sort_list) == test_last_info_data
-    assert len(Transfer().get_last_info(test_sort_list)) == len(test_last_info_data)
